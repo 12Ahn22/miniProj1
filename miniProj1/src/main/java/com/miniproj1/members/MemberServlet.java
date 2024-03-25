82095425a6ca1a10bed87534de0f8abe65ec0caf
12Ahn22 <12ayo22@gmail.com>
2024-03-25 16:00:41 +0900

    [feat] MemberServlet에 로그인 폼 페이지 추가
    Member 서블렛 작성 때, 로그인 폼 페이지를 빼먹음

--- miniProj1/src/main/java/com/miniproj1/members/MemberServlet.java
+++ miniProj1/src/main/java/com/miniproj1/members/MemberServlet.java
@@ -49,6 +49,10 @@ public class MemberServlet extends HttpServlet {
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/members/memberView.jsp");
 			rd.forward(request, response);
 		}
+		case "loginForm" ->{
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/members/login.jsp");
+			rd.forward(request, response);
+		}
 		default -> {
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/notFound.jsp");
 			rd.forward(request, response);
