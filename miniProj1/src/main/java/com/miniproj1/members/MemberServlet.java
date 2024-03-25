8c2f190343fe783aa959d16279035fa671934fe3
12Ahn22 <12ayo22@gmail.com>
2024-03-25 15:01:42 +0900

    [feat] Member JSP 페이지 확인을 위한 Servlet 생성
    URL을 통한 이동 시, 페이지가 브라우저에 출력되도록 함.

---
+++ miniProj1/src/main/java/com/miniproj1/members/MemberServlet.java
@@ -0,0 +1,54 @@
+package com.miniproj1.members;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/member")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public MemberServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doService(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doService(request, response);
+	}
+	
+	private void doService(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 한글 설정
+		request.setCharacterEncoding("utf-8");
+
+		String action = request.getParameter("action");
+
+		switch (action) {
+		case "list" -> {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/members/memberList.jsp");
+			rd.forward(request, response);
+		}
+		case "insertForm" -> {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/members/memberInsert.jsp");
+			rd.forward(request, response);
+		}
+		case "updateForm" -> {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/members/memberUpdate.jsp");
+			rd.forward(request, response);
+		}
+		case "view" -> {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/members/memberView.jsp");
+			rd.forward(request, response);
+		}
+		};
+	}
+}
