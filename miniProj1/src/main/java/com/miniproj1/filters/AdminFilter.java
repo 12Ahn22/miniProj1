731d7d65bd0c7082bc64ae067512cec891711526
12Ahn22 <12ayo22@gmail.com>
2024-03-27 14:21:02 +0900

    [feat] 로그인한 관리자만 list에 접근하는 필터 추가
    - 이전 커밋 : e96d25c474472d499ef437005d81f4f5bba58865에 파일을 추가하지않고 커밋을 진행해서
    다시 커밋함.

---
+++ miniProj1/src/main/java/com/miniproj1/filters/AdminFilter.java
@@ -0,0 +1,54 @@
+package com.miniproj1.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.miniproj1.members.MemberVO;
+
+@WebFilter("/*")
+public class AdminFilter extends HttpFilter implements Filter {
+	private static final long serialVersionUID = 1L;
+
+	public AdminFilter() {
+        super();
+    }
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// member?action=list 페이지는 관리자만 접근이 가능하다.
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		HttpSession session = req.getSession();
+		MemberVO loginMember = (MemberVO) session.getAttribute("loginMember");
+		
+		String action = req.getParameter("action");
+		String uri = req.getRequestURI();
+		
+		if(uri.contains("member")) {
+			if(action != null && action.equals("list")) {
+				// 로그인 여부와 관리자 여부
+				if(loginMember == null || !loginMember.getId().equals("ADMIN")) {
+					res.sendRedirect(req.getContextPath() + "/"); // 리다이렉트
+					return;
+				}
+			}
+		}
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+	}
+
+}
