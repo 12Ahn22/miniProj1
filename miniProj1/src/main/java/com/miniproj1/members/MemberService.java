9ee4437645a1287417b3184ba560d77d7abf5cd2
12Ahn22 <12ayo22@gmail.com>
2024-03-28 09:21:52 +0900

    [fix] 취미를 입력받지 않은 경우 memberHobbyDAO를 호출하지 않도록 변경

--- miniProj1/src/main/java/com/miniproj1/members/MemberService.java
+++ miniProj1/src/main/java/com/miniproj1/members/MemberService.java
@@ -12,8 +12,8 @@ public class MemberService {
 	MemberDAO memberDAO = new MemberDAO(); // 유저 정보
 	HobbyDAO hobbyDAO = new HobbyDAO(); // 취미
 	MemberHobbyDAO memberHobbyDAO = new MemberHobbyDAO(); // 멤버-취미 관계
-	
-	public List<MemberVO> list(){
+
+	public List<MemberVO> list() {
 		// DAO를 호출한다.
 		List<MemberVO> list = memberDAO.list();
 		return list;
@@ -21,8 +21,9 @@ public class MemberService {
 
 	public MemberVO view(MemberVO member) {
 		MemberVO memberVO = memberDAO.view(member);
-		Map<Integer,String> hobbies = memberDAO.getMemberHobbies(member);
-		if(hobbies.size() != 0) memberVO.setHobbies(hobbies);
+		Map<Integer, String> hobbies = memberDAO.getMemberHobbies(member);
+		if (hobbies.size() != 0)
+			memberVO.setHobbies(hobbies);
 		return memberVO;
 	}
 
@@ -35,10 +36,10 @@ public class MemberService {
 	public int update(MemberVO member) {
 		// 멤버-취미 테이블에서 해당 멤버를 전부 삭제
 		memberHobbyDAO.deleteAll(member.getId());
-		
+
 		// 받은 취미를 전부 insert
 		Map<Integer, String> hobbies = member.getHobbies();
-		for(var hobby: hobbies.entrySet()) {
+		for (var hobby : hobbies.entrySet()) {
 			// 실패 처리를 어떻게 해야할까?
 			memberHobbyDAO.insert(member.getId(), hobby.getKey());
 		}
@@ -47,19 +48,19 @@ public class MemberService {
 		return updated;
 	}
 
-	public Map<String,Object> fetchUpdateFormData(MemberVO member) {
-		Map<String,Object> map = new HashMap<String, Object>();
-		
+	public Map<String, Object> fetchUpdateFormData(MemberVO member) {
+		Map<String, Object> map = new HashMap<String, Object>();
+
 		MemberVO memberVO = memberDAO.view(member);
-		Map<Integer,String> memberHobbies = memberDAO.getMemberHobbies(member); // 유저가 선택한 취미 목록
+		Map<Integer, String> memberHobbies = memberDAO.getMemberHobbies(member); // 유저가 선택한 취미 목록
 		memberVO.setHobbies(memberHobbies);
-		
+
 		// Service는 여러 DAO를 가질 수 있음
 		List<HobbyVO> hobbyList = hobbyDAO.list();
-		
+
 		map.put("memberVO", memberVO);
 		map.put("hobbyList", hobbyList);
-		
+
 		return map;
 	}
 
@@ -70,16 +71,19 @@ public class MemberService {
 	public int insert(MemberVO member) {
 		// 멤버를 먼저 생성
 		int updated = memberDAO.insert(member);
-		
+		System.out.println("에러나면 updated는 뭐가 된거지" + updated);
+
 		// 취미-멤버 테이블에 데이터 생성
 		Map<Integer, String> hobbies = member.getHobbies();
-		for(var hobby: hobbies.entrySet()) {
-			memberHobbyDAO.insert(member.getId(), hobby.getKey());
+		if (hobbies != null) {
+			for (var hobby : hobbies.entrySet()) {
+				memberHobbyDAO.insert(member.getId(), hobby.getKey());
+			}
 		}
 		return updated;
 	}
 
 	public boolean authenticateMember(MemberVO member) {
-		return memberDAO.authenticateMember(member);		
+		return memberDAO.authenticateMember(member);
 	}
 }
