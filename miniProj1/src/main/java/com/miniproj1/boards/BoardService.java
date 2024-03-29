be39f12207cc95e1e334c4f36da342460d72197d
12Ahn22 <12ayo22@gmail.com>
2024-03-29 10:17:35 +0900

    [feat] 게시글 서비스 트랜잭션 & 예외 처리

--- miniProj1/src/main/java/com/miniproj1/boards/BoardService.java
+++ miniProj1/src/main/java/com/miniproj1/boards/BoardService.java
@@ -1,35 +1,96 @@
 package com.miniproj1.boards;
 
+import java.sql.SQLException;
 import java.util.List;
+
+import com.miniproj1.baseDAO.BaseDAO;
 
 public class BoardService {
 	BoardDAO boardDAO = new BoardDAO();
 	
-	public List<BoardVO> list(BoardVO boardVO) {
-		return boardDAO.list(boardVO.getSearchKey());
+	public List<BoardVO> list(BoardVO boardVO) throws Exception {
+		List<BoardVO> list = null;
+		try {
+			list = boardDAO.list(boardVO.getSearchKey());
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new Exception();
+		}
+		return list;
 	}
 
 	public BoardVO view(BoardVO boardVO) {
-		// view 카운트를 증가
-		boardDAO.increaseViewCount(boardVO.getBno());
+		BoardVO board = null;
+		try {
+			// view 카운트를 증가
+			boardDAO.increaseViewCount(boardVO.getBno());
+			board = boardDAO.view(boardVO.getBno());
+			BaseDAO.conn.commit();
+		} catch (Exception e) {
+			try {
+				BaseDAO.conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
 		// board 반환
-		return boardDAO.view(boardVO.getBno());
+		return board; // 만약 얘가 없으면 어떻게 처리하지?
 	}
 	
 	public BoardVO fetchUpdateFormData(BoardVO boardVO) {
-		return boardDAO.view(boardVO.getBno());
+		BoardVO board = null;
+		try {
+			board =  boardDAO.view(boardVO.getBno());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return board;
 	}
 
 	public int delete(BoardVO boardVO) {
-		return boardDAO.delete(boardVO.getBno());
+		try {
+			boardDAO.delete(boardVO.getBno());
+			BaseDAO.conn.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				BaseDAO.conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return 0;
+		}
+		return 1;
 	}
 
 	public int update(BoardVO boardVO) {
-		return boardDAO.update(boardVO);
+		try {
+			boardDAO.update(boardVO);
+			BaseDAO.conn.commit();
+		} catch (Exception e) {
+			try {
+				BaseDAO.conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return 0;
+		}
+		return 1;
 	}
 
 	public int insert(BoardVO boardVO) {
-		return boardDAO.insert(boardVO);
+		try {
+			boardDAO.insert(boardVO);
+			BaseDAO.conn.commit();
+		} catch (Exception e) {
+			try {
+				BaseDAO.conn.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			return 0;
+		}
+		return 1;
 	}
 
 }
